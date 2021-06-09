f026a7e24413aa630dc86667dc59cb14fd465c09
Ethan Lipnik <hi@ethanlipnik.com>
2021-06-09 18:11:57 -0400

    Initial Commit

---
+++ Shared/YondaApp.swift
@@ -0,0 +1,20 @@
+//
+//  YondaApp.swift
+//  Shared
+//
+//  Created by Ethan Lipnik on 6/9/21.
+//
+
+import SwiftUI
+
+@main
+struct YondaApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
